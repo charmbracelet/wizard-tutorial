cccce780651ad82e4388ee2c2bcf4df0caa3e9b2
bashbunni <bashbunni.io@gmail.com>
2022-12-09 16:41:12 -0800

    add styling and layout to form

--- main.go
+++ main.go
@@ -8,7 +8,20 @@ import (
 	"github.com/charmbracelet/lipgloss"
 )
 
+type Styles struct {
+	BorderColor lipgloss.Color
+	InputField  lipgloss.Style
+}
+
+func DefaultStyles() *Styles {
+	s := new(Styles)
+	s.BorderColor = lipgloss.Color("36")
+	s.InputField = lipgloss.NewStyle().BorderForeground(s.BorderColor).BorderStyle(lipgloss.NormalBorder()).Padding(1).Width(80)
+	return s
+}
+
 type Main struct {
+	styles      *Styles
 	index       int
 	questions   []string
 	answers     []string
@@ -18,8 +31,10 @@ type Main struct {
 }
 
 func New(questions []string) *Main {
+	styles := DefaultStyles()
 	answerField := textinput.New()
-	return &Main{questions: questions, answerField: answerField}
+	answerField.Placeholder = "Your answer here"
+	return &Main{styles: styles, questions: questions, answerField: answerField}
 }
 
 func (m Main) Init() tea.Cmd {
@@ -44,11 +59,22 @@ func (m Main) View() string {
 	if m.width == 0 {
 		return "loading..."
 	}
-	// stack some strings together in the center of the window
-	return lipgloss.JoinVertical(lipgloss.Center, m.questions[m.index], m.answerField.View())
+	// stack some left-aligned strings together in the center of the window
+	return lipgloss.Place(
+		m.width,
+		m.height,
+		lipgloss.Center,
+		lipgloss.Center,
+		lipgloss.JoinVertical(
+			lipgloss.Left,
+			m.questions[m.index],
+			m.styles.InputField.Render(m.answerField.View()),
+		),
+	)
 }
 
 func main() {
+	// init styles; optional, just showing as a way to organize styles
 	// start bubble tea and init first model
 	questions := []string{"what is your name?", "what is your favourite editor?", "what language do you like most?"}
 	firstQuestion := New(questions)
